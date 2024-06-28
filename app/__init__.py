b7c86e47580b2b260f16bfc48b8f54efdb39d9e0
Aayush Chaudhary <acaayush1111@gmail.com>
2024-06-28 21:46:26 +0530

    gemini api working

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['GEMINI_KEY'] = ''
 
 from .home import home as home_blueprint
 app.register_blueprint(home_blueprint)
--- app/home.py
+++ app/home.py
@@ -1,14 +1,25 @@
 from flask import Blueprint, render_template, request
 
 
+import google.generativeai as genai
+import os
+
+genai.configure(api_key=os.environ['GEMINI_API_KEY'])
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+chat = model.start_chat()
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
 def home_base(): 
-    return render_template('index.html')
+    response = chat.send_message("""Hello, You're Kris. You are a expert scholar of Bhagwat Geeta. You shall not reply with "I am an ai chatbot or anything to do with chatting at all. You must talk like a friend, in simple english. When I share a problem or anything concerning you should reply with a shlok from geeta and reply with some helpful advice as a friend. To start with, you can introduce yourself with Namaste. """)
+
+    return render_template('index.html', response=response)
 
 @home.post('/chat')
-def chat():
+def chat_req():
     query = request.form.get('query')
-    return f"<article>{query}</article>"
+    response = chat.send_message(query)
+    return render_template('chat.response.html',query=query, response=response)
