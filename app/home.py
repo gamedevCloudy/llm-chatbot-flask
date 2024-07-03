310332e5be9429866e7d25b824b34f834e75aa73
Aayush Chaudhary <acaayush1111@gmail.com>
2024-07-03 17:37:06 +0530

    better message formatting

--- app/home.py
+++ app/home.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 
+import html
 
 import google.generativeai as genai
 import os
@@ -14,12 +15,25 @@ home = Blueprint('home', __name__)
 
 @home.route('/')
 def home_base(): 
-    response = chat.send_message("""Hello, You're Kris. You are a expert scholar of Bhagwat Geeta. You shall not reply with "I am an ai chatbot or anything to do with chatting at all. You must talk like a friend, in simple english. When I share a problem or anything concerning you should reply with a shlok from geeta and reply with some helpful advice as a friend. To start with, you can introduce yourself with Namaste. """)
+    response = chat.send_message("""Hello, You're Kris. 
+                                 You are a expert scholar of Bhagwat Geeta. 
+                                 You shall not reply with "I am an ai chatbot or anything to do with chatting at all. 
+                                 You must talk like a friend, in simple english. 
+                                 When I share a problem or anything concerning you should reply with a shlok from geeta and reply with some helpful advice as a friend. 
+                                 
+                                Please respond in HTML only not markdown. 
+                                respone length should be around 30-150 words, excluding sholk. 
+                                To start with, you can introduce yourself with Namaste. """)
 
-    return render_template('index.html', response=response)
+    decoded_response = html.unescape(response.text)
+
+    return render_template('index.html', response=decoded_response)
 
 @home.post('/chat')
 def chat_req():
     query = request.form.get('query')
     response = chat.send_message(query)
-    return render_template('chat.response.html',query=query, response=response)
+
+    decoded_response = html.unescape(response.text)
+
+    return render_template('chat.response.html',query=query, response=decoded_response)
